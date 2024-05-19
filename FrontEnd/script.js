e87253eacd02d1b6868889f978347f8c4ccf2b7a
ThomasCode1 <thomas.adam.perso@gmail.com>
2024-05-19 16:41:41 +0200

    Change of color on active button

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -22,8 +22,13 @@ genererWorks(works)
 let filters = document.querySelector(".filters")
 let tous = document.createElement("button")
 tous.innerText = "Tous"
+let lastButtonClicked = tous
+tous.classList.add("selected-filter")
 tous.addEventListener("click", function () {
     genererWorks(works)
+    lastButtonClicked.classList.remove("selected-filter")
+    tous.classList.add("selected-filter")
+    lastButtonClicked = tous
 })
 filters.appendChild(tous)
 reponse = await fetch("http://localhost:5678/api/categories")
@@ -32,12 +37,14 @@ for(let i = 0;i<categories.length;++i) {
     let button = document.createElement("button")
     button.innerText = categories[i].name
     //button.setAttribute("data-id", categories[i].id)
-    button.classList.add("test")
     button.addEventListener("click", function () {
         const worksFiltered = works.filter(function (work) {
             return work.categoryId == categories[i].id
         })
         genererWorks(worksFiltered)
+        lastButtonClicked.classList.remove("selected-filter")
+        button.classList.add("selected-filter")
+        lastButtonClicked = button
     })
     filters.appendChild(button)
 }
