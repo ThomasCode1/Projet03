45bc981f14b7f49712241f660af0196a386dc1c5
ThomasCode1 <thomas.adam.perso@gmail.com>
2024-06-09 19:34:24 +0200

    Finished project

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,20 +3,20 @@ let token = localStorage.getItem("token")
 /************************************************************************Affichage des works*****************************************************/
 
 function genererWorks(works) {
-    let gallery = document.querySelector(".gallery")
-    gallery.innerHTML= ""
-    for(let work of works) {
-        let imageWork = document.createElement("img");
-        imageWork.src = work.imageUrl
-        imageWork.alt = work.title
-        let caption = document.createElement("figcaption")
-        caption.innerText = work.title
-        let figure = document.createElement("figure")
-        figure.appendChild(imageWork)
-        figure.appendChild(caption)
-        gallery.appendChild(figure)
-    }
-    }
+let gallery = document.querySelector(".gallery")
+gallery.innerHTML= ""
+for(let work of works) {
+    let imageWork = document.createElement("img");
+    imageWork.src = work.imageUrl
+    imageWork.alt = work.title
+    let caption = document.createElement("figcaption")
+    caption.innerText = work.title
+    let figure = document.createElement("figure")
+    figure.appendChild(imageWork)
+    figure.appendChild(caption)
+    gallery.appendChild(figure)
+}
+}
 
 let reponse = await fetch("http://localhost:5678/api/works")
 let works = await reponse.json()
@@ -28,36 +28,36 @@ let optionsAjoutPhoto = document.getElementById("categorieWork")
 reponse = await fetch("http://localhost:5678/api/categories")
 let categories = await reponse.json()
 categories.unshift({
-    id:0,
-    name:"Tous"
+id:0,
+name:"Tous"
 })
 
 function onFilterClick(category) {
-    const button = this
-    const worksFiltered = works.filter(function (work) {
-        return (work.categoryId === category.id) || (category.id === 0)
-    })
-    genererWorks(worksFiltered)
-    let buttons = document.querySelectorAll('.filters button')
-    for(let buttonFilter of buttons) {
-        buttonFilter.classList.remove("selected-filter")
-    }
-    button.classList.add("selected-filter")
+const button = this
+const worksFiltered = works.filter(function (work) {
+    return (work.categoryId === category.id) || (category.id === 0)
+})
+genererWorks(worksFiltered)
+let buttons = document.querySelectorAll('.filters button')
+for(let buttonFilter of buttons) {
+    buttonFilter.classList.remove("selected-filter")
+}
+button.classList.add("selected-filter")
 }
 
 for(let category of categories) {
-    let button = document.createElement("button")
-    button.innerText = category.name
-    if(category.id === 0) {
-        button.classList.add("selected-filter")
-    }
-    button.addEventListener("click", onFilterClick.bind(button, category))
-    filters.appendChild(button)
+let button = document.createElement("button")
+button.innerText = category.name
+if(category.id === 0) {
+    button.classList.add("selected-filter")
+}
+button.addEventListener("click", onFilterClick.bind(button, category))
+filters.appendChild(button)
 if(category.id != 0) {
-    let option = document.createElement("option")
-    option.innerText = category.name
-    option.value = category.id
-    optionsAjoutPhoto.appendChild(option)
+let option = document.createElement("option")
+option.innerText = category.name
+option.value = category.id
+optionsAjoutPhoto.appendChild(option)
 }
 }
 
@@ -72,51 +72,51 @@ let lastMenuClicked = projets
 document.getElementById("login-page").style.display = 'none';
 
 function displayHome() {
-    document.getElementById("introduction").style.display = 'flex';
-    document.getElementById("portfolio").style.display = 'block';
-    document.getElementById("contact").style.display = 'block';
-    document.getElementById("login-page").style.display = 'none';
+document.getElementById("introduction").style.display = 'flex';
+document.getElementById("portfolio").style.display = 'block';
+document.getElementById("contact").style.display = 'block';
+document.getElementById("login-page").style.display = 'none';
 }
 
 projets.addEventListener("click", function () {
-    displayHome()
-    document.getElementById("portfolio").scrollIntoView()
-    lastMenuClicked.classList.remove("selected-menu")
-    projets.classList.add("selected-menu")
-    lastMenuClicked = projets
+displayHome()
+document.getElementById("portfolio").scrollIntoView()
+lastMenuClicked.classList.remove("selected-menu")
+projets.classList.add("selected-menu")
+lastMenuClicked = projets
 })
 contact.addEventListener("click", function () {
-    displayHome()
-    document.getElementById("contact").scrollIntoView()
-    lastMenuClicked.classList.remove("selected-menu")
-    contact.classList.add("selected-menu")
-    lastMenuClicked = contact
+displayHome()
+document.getElementById("contact").scrollIntoView()
+lastMenuClicked.classList.remove("selected-menu")
+contact.classList.add("selected-menu")
+lastMenuClicked = contact
 
 })
 
 login.addEventListener("click", function () {
-    if(token) {
-        token = null
-        localStorage.removeItem("token")
-        login.innerText="login"
-        filters.style.display = 'flex';
-        document.querySelector(".mode-edition").style.display = 'none'
-        document.querySelector(".modifier").style.display = 'none'
-        document.querySelector('.title-modifier').style.marginBottom = '0px'
-    } else {
-    document.getElementById("introduction").style.display = 'none';
-    document.getElementById("portfolio").style.display = 'none';
-    document.getElementById("contact").style.display = 'none';
-    document.getElementById("login-page").style.display = 'block';
-    lastMenuClicked.classList.remove("selected-menu")
-    login.classList.add("selected-menu")
-    lastMenuClicked = login
-    }
+if(token) {
+    token = null
+    localStorage.removeItem("token")
+    login.innerText="login"
+    filters.style.display = 'flex';
+    document.querySelector(".mode-edition").style.display = 'none'
+    document.querySelector(".modifier").style.display = 'none'
+    document.querySelector('.title-modifier').style.marginBottom = '0px'
+} else {
+document.getElementById("introduction").style.display = 'none';
+document.getElementById("portfolio").style.display = 'none';
+document.getElementById("contact").style.display = 'none';
+document.getElementById("login-page").style.display = 'block';
+lastMenuClicked.classList.remove("selected-menu")
+login.classList.add("selected-menu")
+lastMenuClicked = login
+}
 })
 
 const contactForm = document.querySelector('#contact form');
 contactForm.addEventListener("submit", (event) => {
-    event.preventDefault()
+event.preventDefault()
 })
 
 let badLogin = document.querySelector(".badLogin")
@@ -125,39 +125,39 @@ let password = document.getElementById("password")
 const loginForm = document.querySelector('.login-form');
 
 function successLogin(token) {
-    localStorage.setItem("token", token)
-    badLogin.innerText = ""
-    displayHome()
-    document.querySelector('.mode-edition').style.display = 'flex'
-    document.querySelector('.modifier').style.display = 'block'
-    document.querySelector('.title-modifier').style.marginBottom = '110px'
-    document.querySelector("body").scrollIntoView()
-    lastMenuClicked.classList.remove("selected-menu")
-    projets.classList.add("selected-menu")
-    lastMenuClicked = projets
-    login.innerText="logout"
-    filters.style.display = 'none'
+localStorage.setItem("token", token)
+badLogin.innerText = ""
+displayHome()
+document.querySelector('.mode-edition').style.display = 'flex'
+document.querySelector('.modifier').style.display = 'block'
+document.querySelector('.title-modifier').style.marginBottom = '110px'
+document.querySelector("body").scrollIntoView()
+lastMenuClicked.classList.remove("selected-menu")
+projets.classList.add("selected-menu")
+lastMenuClicked = projets
+login.innerText="logout"
+filters.style.display = 'none'
 }
 
 if(token) {
-    successLogin(token)
+successLogin(token)
 }
 
 let loginResult
 loginForm.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    reponse = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({"email": email.value,"password": password.value})
-    })
-    loginResult = await reponse.json()
-        if(Object.keys(loginResult).length === 2) {
-            token = loginResult.token
-           successLogin(token)
-        } else {
-            badLogin.innerText = "Erreur dans l’identifiant ou le mot de passe"
-        }
+event.preventDefault()
+reponse = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({"email": email.value,"password": password.value})
+})
+loginResult = await reponse.json()
+    if(Object.keys(loginResult).length === 2) {
+        token = loginResult.token
+        successLogin(token)
+    } else {
+        badLogin.innerText = "Erreur dans l’identifiant ou le mot de passe"
+    }
 })
 
 
@@ -170,59 +170,59 @@ let closeTwo = document.querySelector(".close-two")
 let worksModal = document.querySelector(".works-modale")
 
 buttonModal.onclick = function() {
-  modal.style.display = "block"
+modal.style.display = "block"
 }
 
 close.onclick = function() {
-    modal.style.display = "none"
-  }
+modal.style.display = "none"
+}
 
-  closeTwo.onclick = function() {
-    modal.style.display = "none"
-  }
+closeTwo.onclick = function() {
+modal.style.display = "none"
+}
 
 window.onclick = function(event) {
-  if (event.target === modal) {
-    modal.style.display = "none"
-  }
+if (event.target === modal) {
+modal.style.display = "none"
+}
 }
 
 function genererWorksModal() {
 for(const work of works) {
-    let imageWork = document.createElement("img");
-    imageWork.src = work.imageUrl
-    imageWork.alt = work.title
-    let figure = document.createElement("figure")
-    figure.appendChild(imageWork)
-    let buttonRemove = document.createElement("div")
-    buttonRemove.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-     buttonRemove.addEventListener("click", async (event) => {
-        reponse = await fetch(`http://localhost:5678/api/works/${work.id}`, {
-            method: "DELETE",
-            headers: { "Content-Type": "application/json", 'Authorization': `Bearer ${token}` }
-        })
-        worksModal.innerHTML = ""
-        reponse = await fetch("http://localhost:5678/api/works")
-        works = await reponse.json()
-        genererWorks(works)
-        genererWorksModal()
+let imageWork = document.createElement("img")
+imageWork.src = work.imageUrl
+imageWork.alt = work.title
+let figure = document.createElement("figure")
+figure.appendChild(imageWork)
+let buttonRemove = document.createElement("div")
+buttonRemove.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    buttonRemove.addEventListener("click", async (event) => {
+    reponse = await fetch(`http://localhost:5678/api/works/${work.id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json", 'Authorization': `Bearer ${token}` }
     })
-    buttonRemove.classList.add("button-remove")
-    figure.appendChild(buttonRemove)
-    worksModal.appendChild(figure)
+    worksModal.innerHTML = ""
+    reponse = await fetch("http://localhost:5678/api/works")
+    works = await reponse.json()
+    genererWorks(works)
+    genererWorksModal()
+})
+buttonRemove.classList.add("button-remove")
+figure.appendChild(buttonRemove)
+worksModal.appendChild(figure)
 }
 }
 
 genererWorksModal()
 
 document.querySelector(".button-ajout-photo").addEventListener("click", (event) => {
-    document.querySelector(".ajout-photo").style.display = 'flex'
-    document.querySelector(".modal-content").style.display = 'none'
-    })
+document.querySelector(".ajout-photo").style.display = 'flex'
+document.querySelector(".modal-content").style.display = 'none'
+})
 
 document.querySelector(".retour-modale").addEventListener("click", (event) => {
-    document.querySelector(".ajout-photo").style.display = 'none'
-    document.querySelector(".modal-content").style.display = 'flex'
+document.querySelector(".ajout-photo").style.display = 'none'
+document.querySelector(".modal-content").style.display = 'flex'
 })
 
 
@@ -232,23 +232,40 @@ let categorieWork = document.getElementById("categorieWork")
 let imageWork = document.getElementById("imageWork")
 let messageUpload = document.querySelector(".message-upload")
 const formWorkUpload = document.querySelector(".upload-form")
+
+imageWork.addEventListener('change', function(event) {
+if (event.target.files[0]) {
+    let file = event.target.files[0]
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = readerEvent => {
+    let imageWorkUpload = document.createElement("img")
+    imageWorkUpload.src = readerEvent.target.result
+    imageWorkUpload.alt = event.target.files[0].name
+    imageWorkUpload.classList.add("imageWorkUpload")
+    document.querySelector(".imageWorkZone").appendChild(imageWorkUpload)
+    document.querySelector(".button-photo").style.display = "none"
+}
+}
+})
+
 formWorkUpload.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    if((titreWork.value === "") || (categorieWork.value === null) || (imageWork.value === "")) {
+event.preventDefault()
+if((titreWork.value === "") || (categorieWork.value === null) || (imageWork.value === "")) {
 messageUpload.innerText = "Tous les champs doivent être remplis"
-    } else {
-    const formData = new FormData(event.target)
-    reponse = await fetch(`http://localhost:5678/api/works`, {
-        method: "POST",
-        headers: {'Authorization': `Bearer ${token}` },
-        body: formData
-    })
-    }
-    let result = await reponse.json()
-    messageUpload.innerText = "La photo a bien été envoyée!"
-    worksModal.innerHTML = ""
-    reponse = await fetch("http://localhost:5678/api/works")
-    works = await reponse.json()
-    genererWorks(works)
-    genererWorksModal()
+} else {
+const formData = new FormData(event.target)
+reponse = await fetch(`http://localhost:5678/api/works`, {
+    method: "POST",
+    headers: {'Authorization': `Bearer ${token}` },
+    body: formData
+})
+}
+let result = await reponse.json()
+messageUpload.innerText = "La photo a bien été envoyée!"
+worksModal.innerHTML = ""
+reponse = await fetch("http://localhost:5678/api/works")
+works = await reponse.json()
+genererWorks(works)
+genererWorksModal()
 })
